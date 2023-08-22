6510458227f18bb1ae6221f7ca9943157fdd03f2
khalid khatkhour <kharkhour@hotmail.fr>
2023-08-22 14:32:26 +0200

    install site

---
+++ public/themes/wordplate/index.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<main>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article>
+                <header>
+                    <h1><?php the_title(); ?></h1>
+                </header>
+
+                <?php the_content(); ?>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <article>
+            <p class="ffa" >Nothing to see.</p>
+          
+        </article>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
